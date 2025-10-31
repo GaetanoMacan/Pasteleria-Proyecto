5cceb52de53644c89cf915288ee60709157433ef
GaetanoMacan <gaetanomacan@gmail.com>
2025-10-31 19:39:57 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const boton = document.getElementById("boton");
+    const modal = document.getElementById("modal");
+    const cerrar = document.getElementById("cerrar");
+  
+    boton.addEventListener("click", () => {
+      modal.classList.remove("oculto");
+    });
+  
+    boton.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      modal.classList.remove("oculto");
+    });
+  
+    cerrar.addEventListener("click", () => {
+      modal.classList.add("oculto");
+    });
+  });
+
+  
