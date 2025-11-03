6fb4bf7f278cf9119f5fd83e690273c78f8333ed
GaetanoMacan <gaetanomacan@gmail.com>
2025-11-03 08:29:49 -0300

    Corrijo 2 modal

--- Script.js
+++ Script.js
@@ -1,20 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const boton = document.getElementById("boton");
-    const modal = document.getElementById("modal");
-    const cerrar = document.getElementById("cerrar");
-  
-    boton.addEventListener("click", () => {
-      modal.classList.remove("oculto");
-    });
-  
-    boton.addEventListener("contextmenu", (e) => {
-      e.preventDefault();
-      modal.classList.remove("oculto");
-    });
-  
-    cerrar.addEventListener("click", () => {
-      modal.classList.add("oculto");
-    });
+  const boton = document.getElementById("boton");
+  const modal = document.getElementById("modal");
+  const cerrar = document.getElementById("cerrar");
+
+  boton.addEventListener("click", () => {
+    modal.classList.remove("oculto");
   });
 
-  
+  boton.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    modal.classList.remove("oculto");
+  });
+
+  cerrar.addEventListener("click", () => {
+    modal.classList.add("oculto");
+  });
+});
